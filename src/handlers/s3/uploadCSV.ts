a47950115e1699ac0682aa44ecd13539a9afa5f4
Nathan Walton <nathan@waltonwebservices.com>
2022-07-18 12:11:57 +0100

    S3 event for csv upload

--- src/handlers/s3/uploadCSV.ts
+++ src/handlers/s3/uploadCSV.ts
@@ -1,25 +1,37 @@
 /* eslint-disable import/no-import-module-exports */
 import csv from 'csvtojson';
 import multipart from 'parse-multipart';
+import { S3 } from 'aws-sdk'
 import { StatusCodes } from 'http-status-codes';
-import { APIGatewayProxyEvent, Callback, Context } from 'aws-lambda';
+import { Callback, Context } from 'aws-lambda';
 import { DocumentClient, WriteRequests } from 'aws-sdk/clients/dynamodb';
 import { insertCSVData } from '../../repository/service';
 import batchItems from '../../helpers';
 import config from '../../config';
 
+const s3 = new S3({ apiVersion: '2006-03-01' });
+
 // eslint-disable-next-line func-names
-module.exports.handler = async function (event: APIGatewayProxyEvent, context: Context, callback: Callback<any>) {
+module.exports.handler = async function (event: any, context: Context, callback: Callback<any>) {
   try {
+		const bucket = event.Records[0].s3.bucket.name;
+
+		const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+
+		const params = {
+        Bucket: bucket,
+        Key: key,
+    };
+
+		const { Body: s3Data } = await s3.getObject(params).promise();
+
+		console.log('BODY:', s3Data)
+
 		const { tableName } = config.dynamodb;
 
-    const body = Buffer.from(event.body as string);
+    const body = Buffer.from(s3Data as string);
 
-    const boundary = multipart.getBoundary(event.headers['Content-Type'] as string);
-
-    const buff = multipart.Parse(body, boundary);
-
-    const csvDataString = buff[0].data.toString('utf8');
+		const csvDataString = body.toString('utf-8');
 
     const csvData = await csv().fromString(csvDataString);
 
